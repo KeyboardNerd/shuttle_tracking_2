0aa0475099919bfb2485a32b516869df8d1c9ba3
connection-lost <frankwu1995@hotmail.com>
2016-12-12 10:25:00 -0500

    update file with new format

--- main.go
+++ main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-	"net/http"
-	"shuttle_tracking_2/tracking"
+  "net/http"
+  "shuttle_tracking_2/tracking"
 
-	log "github.com/Sirupsen/logrus"
-	"github.com/gorilla/mux"
+  log "github.com/Sirupsen/logrus"
+  "github.com/gorilla/mux"
 )
 
 var (
-	// Config holds the global app settings.
-	Config = tracking.InitConfig()
-	// App holds the application itself.
-	App = tracking.InitApp(Config)
+  // Config holds the global app settings.
+  Config = tracking.InitConfig()
+  // App holds the application itself.
+  App = tracking.InitApp(Config)
 )
 
 // IndexHandler serves the index page.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+  http.ServeFile(w, r, "index.html")
 }
 
 // AdminHandler serves the admin page.
 func AdminHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "admin.html")
+  http.ServeFile(w, r, "admin.html")
 }
 
 func main() {
-	// close Mongo session when server terminates
-	defer App.Session.Close()
+  // close Mongo session when server terminates
+  defer App.Session.Close()
 
-	// Start auto updater
-	go App.UpdateShuttles(Config.DataFeed, Config.UpdateInterval)
+  // Start auto updater
+  go App.UpdateShuttles(Config.DataFeed, Config.UpdateInterval)
 
-	// Routing
-	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler).Methods("GET")
-	r.HandleFunc("/admin", AdminHandler).Methods("GET")
-	r.HandleFunc("/admin/{*}", AdminHandler).Methods("GET")
-	r.HandleFunc("/vehicles", App.VehiclesHandler).Methods("GET")
-	r.HandleFunc("/vehicles/create", App.VehiclesCreateHandler).Methods("POST")
-	r.HandleFunc("/vehicles/edit", App.VehiclesEditHandler).Methods("POST")
-	r.HandleFunc("/vehicles/{id:[0-9]+}", App.VehiclesDeleteHandler).Methods("DELETE")
-	r.HandleFunc("/updates", App.UpdatesHandler).Methods("GET")
-	r.HandleFunc("/updates/message", App.UpdateMessageHandler).Methods("GET")
-	r.HandleFunc("/routes", App.RoutesHandler).Methods("GET")
-	r.HandleFunc("/routes/create", App.RoutesCreateHandler).Methods("POST")
-	r.HandleFunc("/routes/{id:.+}", App.RoutesDeleteHandler).Methods("DELETE")
-	r.HandleFunc("/stops", App.StopsHandler).Methods("GET")
-	r.HandleFunc("/stops/create", App.StopsCreateHandler).Methods("POST")
-	r.HandleFunc("/stops/{id:.+}", App.StopsDeleteHandler).Methods("DELETE")
-	// Static files
-	r.PathPrefix("/bower_components/").Handler(http.StripPrefix("/bower_components/", http.FileServer(http.Dir("bower_components/"))))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
-	// Serve requests
-	http.Handle("/", r)
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatalf("Unable to ListenAndServe: %v", err)
-	}
+  // Routing
+  r := mux.NewRouter()
+  r.HandleFunc("/", IndexHandler).Methods("GET")
+  r.HandleFunc("/admin", AdminHandler).Methods("GET")
+  r.HandleFunc("/admin/{*}", AdminHandler).Methods("GET")
+  r.HandleFunc("/vehicles", App.VehiclesHandler).Methods("GET")
+  r.HandleFunc("/vehicles/create", App.VehiclesCreateHandler).Methods("POST")
+  r.HandleFunc("/vehicles/edit", App.VehiclesEditHandler).Methods("POST")
+  r.HandleFunc("/vehicles/{id:[0-9]+}", App.VehiclesDeleteHandler).Methods("DELETE")
+  r.HandleFunc("/updates", App.UpdatesHandler).Methods("GET")
+  r.HandleFunc("/updates/message", App.UpdateMessageHandler).Methods("GET")
+  r.HandleFunc("/routes", App.RoutesHandler).Methods("GET")
+  r.HandleFunc("/routes/create", App.RoutesCreateHandler).Methods("POST")
+  r.HandleFunc("/routes/{id:.+}", App.RoutesDeleteHandler).Methods("DELETE")
+  r.HandleFunc("/stops", App.StopsHandler).Methods("GET")
+  r.HandleFunc("/stops/create", App.StopsCreateHandler).Methods("POST")
+  r.HandleFunc("/stops/{id:.+}", App.StopsDeleteHandler).Methods("DELETE")
+  // Static files
+  r.PathPrefix("/bower_components/").Handler(http.StripPrefix("/bower_components/", http.FileServer(http.Dir("bower_components/"))))
+  r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+  // Serve requests
+  http.Handle("/", r)
+  if err := http.ListenAndServe(":8080", r); err != nil {
+    log.Fatalf("Unable to ListenAndServe: %v", err)
+  }
 }
